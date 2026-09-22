019714731b4d7c2f0c71a398bb21d01fab7b9237
KutikiPlayz <kutikiplayz@gmail.com>
2026-09-22 02:41:19 -0600

    basic info n shit

--- src/main/java/coolguy/kutikiplayz/CodeBreakerOrigin.java
+++ src/main/java/coolguy/kutikiplayz/CodeBreakerOrigin.java
@@ -9,22 +9,9 @@ import org.slf4j.LoggerFactory;
 
 public class CodeBreakerOrigin implements ModInitializer {
 	public static final String MOD_ID = "code-breaker-origin";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+	@Override public void onInitialize() {}
 
-		LOGGER.info("Hello Fabric world!");
-	}
-
-	public static Identifier id(String path) {
-		return Identifier.of(MOD_ID, path);
-	}
+	public static Identifier id(String path) { return Identifier.of(MOD_ID, path); }
 }
